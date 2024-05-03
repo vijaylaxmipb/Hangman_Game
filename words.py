ee8614f20fc594d3fa10816978715484b87fb50f
Vijaylaxmi Biradar <vijaylaxmipb@gmail.com>
2024-05-03 14:47:10 +0000

    Create words file and add wordlist to thr file

---
+++ words.py
@@ -0,0 +1,119 @@
+wordlist = [
+    'azure',
+    'pizza',
+    'pasta',
+    'pandemic',
+    'olympics',
+    'protests',
+    'elections',
+    'beijing',
+    'moscow',
+    'sydney',
+    'cairo',
+    'Mumbai',
+    'berlin',
+    'cheese',
+    'cookie',
+    'smoothie',
+    'advise',
+    'adviser',
+    'advocate',
+    'affair',
+    'affect',
+    'afford',
+    'afraid',
+    'capable',
+    'capacity',
+    'capital',
+    'captain',
+    'capture',
+    'downtown',
+    'dozen',
+    'enforcement',
+    'energy',
+    'explosion',
+    'extensive',
+    'extent',
+    'feature',
+    'federal',
+    'fighter',
+    'finger',
+    'furniture',
+    'guard',
+    'guest',
+    'guilty',
+    'guideline',
+    'headline',
+    'heritage',
+    'highlight',
+    'humor',
+    'hypothesis',
+    'incentive',
+    'incident',
+    'incredible',
+    'institution',
+    'intense',
+    'journal',
+    'laboratory',
+    'league',
+    'legacy',
+    'literature',
+    'machine',
+    'massive',
+    'obligation',
+    'occasion',
+    'obvious',
+    'orientation',
+    'package',
+    'particular',
+    'perspective',
+    'persuade',
+    'portray',
+    'pride',
+    'beginner',
+    'behave',
+    'behaviour',
+    'belief',
+    'believe',
+    'bell',
+    'belligerent',
+    'bells',
+    'belong',
+    'remarkable',
+    'religion',
+    'repeat',
+    'reproduce',
+    'repulsive',
+    'request',
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+]
